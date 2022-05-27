905cded6af3d6715a8790a40519407aa29042f5b
Ksenia-Ksu <kseniakobak@gmail.com>
2022-05-27 23:25:00 +0300

    Initial Commit

---
+++ ByteCoinProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoinProject
+//
+//  Created by Ксения Кобак on 27.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
